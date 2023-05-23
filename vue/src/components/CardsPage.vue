142693c8fa58e6c54581742f7b9766e70ceffe52
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2023-05-23 09:47:56 -0400

    Update CardsPage.vue

--- vue/src/components/CardsPage.vue
+++ vue/src/components/CardsPage.vue
@@ -1,3 +1,10 @@
+<template>
+  <div class="card">
+    <h2 class="name">{{ Characters }}</h2>
+    <img class="img" :src="img_url" />
+  </div>
+</template>
+
 <script setup>
 import { supabase } from '../supabase'
 import { onMounted, ref, toRefs } from 'vue'
@@ -6,58 +13,30 @@ const props = defineProps(['session'])
 const { session } = toRefs(props)
 
 const loading = ref(true)
-const username = ref('')
-const website = ref('')
-const avatar_url = ref('')
-const favChar = ref('')
+const Characters = ref(Characters)
+const img_url = ref(img_url)
 
 onMounted(() => {
-  getProfile()
+  displayCharacters()
 })
 
-async function getProfile() {
+async function displayCharacters() {
   try {
     loading.value = true
     const { user } = session.value
 
     let { data, error, status } = await supabase
-      .from('profiles')
-      .select(`username, website, avatar_url, favChar`)
+      .from('Players')
+      .select(`Characters, img_url`)
       .eq('id', user.id)
       .single()
 
     if (error && status !== 406) throw error
 
     if (data) {
-      username.value = data.username
-      website.value = data.website
-      avatar_url.value = data.avatar_url
-      favChar.value = data.favChar
+      Characters.value = data.Characters
+      img_url.value = data.img_url
     }
-  } catch (error) {
-    alert(error.message)
-  } finally {
-    loading.value = false
-  }
-}
-
-async function updateProfile() {
-  try {
-    loading.value = true
-    const { user } = session.value
-
-    const updates = {
-      id: user.id,
-      username: username.value,
-      website: website.value,
-      favChar: favChar.value,
-      avatar_url: avatar_url.value,
-      updated_at: new Date()
-    }
-
-    let { error } = await supabase.from('profiles').upsert(updates)
-
-    if (error) throw error
   } catch (error) {
     alert(error.message)
   } finally {
@@ -77,37 +56,3 @@ async function signOut() {
   }
 }
 </script>
-
-<template>
-  <form class="form-widget" @submit.prevent="updateProfile">
-    <div>
-      <label for="email">Email: </label>
-      <input id="email" type="text" :value="session.user.email" disabled />
-    </div>
-    <div>
-      <label for="username">Name: </label>
-      <input id="username" type="text" v-model="username" />
-    </div>
-    <div>
-      <label for="website">Website: </label>
-      <input id="website" type="url" v-model="website" />
-    </div>
-    <div>
-      <label for="favChar">Favorite Character: </label>
-      <input id="favChar" type="text" v-model="favChar" />
-    </div>
-
-    <div>
-      <input
-        type="submit"
-        class="button primary block"
-        :value="loading ? 'Loading ...' : 'Update'"
-        :disabled="loading"
-      />
-    </div>
-
-    <div>
-      <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
-    </div>
-  </form>
-</template>
