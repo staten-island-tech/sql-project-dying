7387f1578197be546fde3ded48bde958c054207d
Sophie Dong <sophiedong2007@gmail.com>
2023-05-26 10:15:33 -0400

    .

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -27,7 +27,6 @@ onMounted(() => {
   <div class="container" style="padding: 50px 0 100px 0">
     <Account v-if="session" :session="session" />
     <Auth v-else />
-    <NavTab />
   </div>
 </template>
 
