ec34fc2484fc225dd7cd282bf093e54dddba6eee
Sophie Dong <sophiedong2007@gmail.com>
2023-06-08 17:01:00 -0400

    a

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -22,6 +22,7 @@ onMounted(() => {
   <div class="container" style="padding: 50px 0 100px 0">
     <Account v-if="session" :session="session" />
     <Auth v-else />
+    <RouterView />
   </div>
 </template>
 
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -11,6 +11,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
       path: '/store',
       name: 'store',
       component: Store
